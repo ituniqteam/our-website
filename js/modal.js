14b04d515342df53795a294700a0283a99e1826e
Alibek <alibek.fras@gmail.com>
2023-01-21 18:52:48 +0600

    request test v10

--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,8 @@
-const contactModal = document.querySelector(".contact-us-modal-wrapper")
-const openModalBtn1 = document.querySelector("#open-modal1")
-const openModalBtn2 = document.querySelector("#open-modal2")
+const contactModal = document.querySelector(".contact-us-modal-wrapper");
+const openModalBtn1 = document.querySelector("#open-modal1");
+const openModalBtn2 = document.querySelector("#open-modal2");
+const sendFormBtn = document.querySelector('.send-form')
+const requestHasBeenSent = document.querySelector('.request-has-been-sent');
 
 openModalBtn1.addEventListener('click', () => {
     contactModal.style.display = "block"
@@ -15,3 +17,8 @@ window.onclick = (e) => {
         contactModal.style.display = "none";
     }
 }
+
+sendFormBtn.addEventListener('click', () => {
+    requestHasBeenSent.style.transform = "translateX(0)"
+    setTimeout(() => requestHasBeenSent.style.transform = "translateX(200%)", 1500)
+})
---
+++ mail.php
@@ -0,0 +1,40 @@
+<?php 
+
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+$name = $_POST['name'];
+$surname = $_POST['surname']
+$email = $_POST['email'];
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'alibek.fras@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'oOAryajAR11%'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
+
+$mail->setFrom($email); // от кого будет уходить письмо?
+$mail->addAddress('alibek.fras@gmail.com');     // Кому будет уходить письмо 
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Заявка с тестового сайта';
+$mail->Body    = '' .$name .$surname. ' оставил заявку<br>Почта этого пользователя: ' .$email;
+$mail->AltBody = '';
+
+if(!$mail->send()) {
+    echo 'Error';
+} else {
+    header('location: thank-you.html');
+}
+?>
