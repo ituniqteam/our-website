618adb5875d60392e6c115551450f8591b91e0c0
Alibek <alibek.fras@gmail.com>
2023-01-22 16:45:45 +0600

    request test v14

--- mail.php
+++ mail.php
@@ -1,41 +1,44 @@
-<?php 
+<?php
 
-require_once('phpmailer/PHPMailerAutoload.php');
-$mail = new PHPMailer;
-$mail->CharSet = 'utf-8';
+ini_set('display_errors','On');
+error_reporting('E_ALL');
 
-$name = $_POST['name'];
-$surname = $_POST['surname']
-$email = $_POST['email'];
-$text = $_POST['text']
+$to = 'alibek.fras@gmail.com'; //Адреса, куда будут приходить письма. две почты указываем через запятую
+$sitename = $_SERVER['SERVER_NAME'];
 
-//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+if (isset($_POST['email']) && !empty($_POST['email']))
+{
+    $name  = strip_tags($_POST['name']);
+    $surname  = strip_tags($_POST['surname']);
+    $email  = strip_tags($_POST['email']);
+    $message  = strip_tags($_POST['message']);
 
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.gmail.com';  																							// Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'alibek.fras@gmail.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = 'i.just.want.to.be.happy0771'; // Ваш пароль от почты с которой будут отправляться письма
-$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
-
-$mail->setFrom($email); // от кого будет уходить письмо?
-$mail->addAddress('alibek.fras@gmail.com');     // Кому будет уходить письмо 
-//$mail->addAddress('ellen@example.com');               // Name is optional
-//$mail->addReplyTo('info@example.com', 'Information');
-//$mail->addCC('cc@example.com');
-//$mail->addBCC('bcc@example.com');
-//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-$mail->isHTML(true);                                  // Set email format to HTML
-
-$mail->Subject = 'Заявка с тестового сайта';
-$mail->Body    = '' .$name .$surname. ' оставил заявку<br>Почта этого пользователя: ' .$email. '<br> Информация о проекте: ' .$text;
-$mail->AltBody = '';
-
-if(!$mail->send()) {
-    echo 'Error';
-} else {
-    header('location: thank-you.html');
+// Формирование заголовка письма
+    $subject  = "[Zajavka s sajta ".$sitename."]";
+    $headers  = "From: mail@".$sitename." \r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html;charset=utf-8 \r\n";
+// Формирование тела письма
+    $msg  = "<html><body style='font-family:Arial,sans-serif;'>";
+    $msg .= "<h2 style='font-weight:bold;border-bottom:1px dotted #ccc;'>Новая заявка:</h2>\r\n";
+    if(isset($_POST['name']) && !empty($_POST['name'])){
+      $msg .= "<p><strong>Имя:</strong> ".$name."</p>\r\n";
+    }
+    if(isset($_POST['surname']) && !empty($_POST['surname'])){
+      $msg .= "<p><strong>Фамилия:</strong> ".$surname."</p>\r\n";
+    }
+    if(isset($_POST['email']) && !empty($_POST['email'])){
+      $msg .= "<p><strong>E-mail:</strong> ".$email."</p>\r\n";
+    }
+    if(isset($_POST['message']) && !empty($_POST['message'])){
+      $msg .= "<p><strong>Сообщение:</strong> ".$message."</p>\r\n";
+    }
+    $msg .= "</body></html>";
+// отправка сообщения
+    mail($to, $subject, $msg, $headers);
+}
+else
+{
+    echo "Заявка не отправлена :(";
 }
 ?>
