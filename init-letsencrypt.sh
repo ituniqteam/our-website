26c5eb696447b18ddce6644750fa080dbc1f9473
ituniqteam <121553264+ituniqteam@users.noreply.github.com>
2023-01-23 22:36:09 +0600

    Update init-letsencrypt.sh

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains= ( itunique.net )
+domains=itunique.net 
 rsa_key_size=4096
 data_path="./data/certbot"
 email="simenshteyn@gmail.com" # Adding a valid address is strongly recommended
